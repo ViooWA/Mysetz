67e5af5696380a812f171bb6a03f7d65a0266f35
ViooWA <vioobot@gmail.com>
2024-11-24 15:36:48 +0700

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
             return res.status(200).json({
                 status: true,
-                result: response.result,
+                result: response.data,
             });
         } catch (err) {
             console.error("External API Error:", err.message);
