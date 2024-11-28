cc468ca3f6ebb6bd59db93f3596530e38d69a507
ViooWA <vioobot@gmail.com>
2024-11-28 23:47:26 +0700

    Add files via upload

--- api.js
+++ api.js
@@ -31,13 +31,12 @@ throw new Error('CarbonifyV2 failed: ' + err.message);
 }}
 
 async function handler(req, res) {
-const { tag, text, avatar, username, url } = req.query;
+const { tag, text, text1, avatar, username, url } = req.query;
 
 try {
 if (tag === 'openai') { // OPENAI
 const response = await axios.get(`https://api.agatz.xyz/api/gpt4o?message=${encodeURIComponent(text)}`
 );
-
 return res.status(200).json({
 status: true,
 result: response.data.data.result,
@@ -48,33 +47,42 @@ content: text,
 cName: "S-AI",
 cID: "S-AIbAQ0HcC"
 };
-
 const response = await axios.post('https://luminai.my.id/', requestData);
 const sai = response.data;
 const pe = sai.result;
-
 return res.status(200).json({
 status: true,
 result: pe,
 });
 } else if (tag === 'luminai') { // LUMINAI
 const response = await axios.post('https://luminai.my.id/', { content: text });
-
 return res.status(200).json({
 status: true,
 result: response.data.result,
 });
 } else if (tag === 'simisimi') { // SIMISIMI
 const response = await axios.get(`https://api.vreden.my.id/api/simi?query=${encodeURIComponent(text)}&lang=id`);
-
 return res.json({
+status: true,
+result: response.data.result,
+});
+} else if (tag === 'gemini') { // GEMINI
+const response = await axios.get(`https://api.agatz.xyz/api/gemini?message=${encodeURIComponent(text)}`
+);
+return res.status(200).json({
+status: true,
+result: response.data.data.answer,
+});
+} else if (tag === 'morai') { // MORAI
+const response = await axios.get(`https://api.vreden.my.id/api/mora?query=${encodeURIComponent(text)}&username=${encodeURIComponent(username)}`
+);
+return res.status(200).json({
 status: true,
 result: response.data.result,
 });
 } else if (tag === 'google') { // GOOGLE
 const response = await axios.get(`https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(text)}&key=AIzaSyAajE2Y-Kgl8bjPyFvHQ-PgRUSMWgBEsSk&cx=e5c2be9c3f94c4bbb`);
 const items = response.data.items;
-
 if (items && items.length > 0) {
 return res.json({
 status: true,
@@ -104,7 +112,6 @@ version: pkg.version,
 npm: pkg.links.npm,
 description: pkg.description,
 }));
-
 return res.json({
 status: true,
 data,
@@ -114,26 +121,37 @@ const response = await axios.get(`https://itzpire.com/search/pinterest?query=${e
 if (response.data.status !== "success") {
 return res.json({
 status: false,
-result: 'Error: API returned failure'
+data: 'Error: API returned failure'
 });
 }
-
 return res.json({
 status: true,
-result: response.data.data,
+data: response.data.data,
 });
 } else if (tag === 'wikimedia') { // WIKIMEDIA
 const response = await axios.get(`https://itzpire.com/search/wikimedia?query=${encodeURIComponent(text)}`);
 if (response.data.status !== "success") {
 return res.json({
 status: false,
-result: 'Error: API returned failure'
-});
-}
-
+data: 'Error: API returned failure'
+})}
 return res.json({
 status: true,
-result: response.data.data,
+data: response.data.data,
+});
+} else if (tag === 'playstore') { // PLAYSTORE
+const response = await axios.get(`https://api.agatz.xyz/api/playstore?message=${encodeURIComponent(text)}`
+);
+return res.status(200).json({
+status: true,
+data: response.data.data,
+});
+} else if (tag === 'happymod') { // HAPPYMOD
+const response = await axios.get(`https://itzpire.com/search/happymod?query=${encodeURIComponent(text)}`
+);
+return res.status(200).json({
+status: true,
+data: response.data.data,
 });
 } else if (tag === 'brat') { // BRAT
 const response = await axios.get(
@@ -155,10 +173,8 @@ const response = await axios.get(
 `https://api.siputzx.my.id/api/m/carbonify?input=${encodeURIComponent(text)}`,
 { responseType: 'arraybuffer' }
 );
-
 res.setHeader('Content-Type', 'image/png');
 res.send(response.data);
-
 } catch (err) {
 try {
 const buffer = await CarbonifyV1(text);
@@ -172,7 +188,20 @@ res.send(buffer);
 } else if (tag === 'txtimg') { // TXTIMG
 const apiUrl = `https://dummyimage.com/600x400/000/fff&text=${encodeURIComponent(text)}`;
 const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
-
+res.setHeader('Content-Type', 'image/png');
+res.send(response.data);
+} else if (tag === 'memegen') { // MEMEGEN
+const response = await axios.get(
+`https://api.siputzx.my.id/api/m/memgen?link=${url}&top=${encodeURIComponent(text)}&bottom=${encodeURIComponent(text1)}&font=1`,
+{ responseType: 'arraybuffer' }
+);
+res.setHeader('Content-Type', 'image/png');
+res.send(response.data);
+} else if (tag === 'blurimg') { // BLURIMG
+const response = await axios.get(
+`https://api.siputzx.my.id/api/m/blur?url=${url}`,
+{ responseType: 'arraybuffer' }
+);
 res.setHeader('Content-Type', 'image/png');
 res.send(response.data);
 } else if (tag === 'mediafire') { // MEDIAFIRE
@@ -181,15 +210,14 @@ const data = response.data;
 if (!data.result || data.result.length === 0) {
 return res.json({
 status: false,
-result: 'Tidak ada hasil ditemukan',
+data: 'Tidak ada hasil ditemukan',
 });
 }
 const fileNama = decodeURIComponent(data.result[0].nama);
 const fileLink = data.result[0].link;
-
 return res.json({
 status: true,
-result: {
+data: {
 fileName: fileNama,
 url: fileLink,
 }, });
@@ -199,7 +227,7 @@ const data = response.data;
 if (!data.result || !data.result.data || data.result.data.length === 0) {
 return res.json({
 status: false,
-result: 'Tidak ada hasil ditemukan',
+data: 'Tidak ada hasil ditemukan',
 })}
 let videoUrl = null;
 for (let item of data.result.data) {
@@ -207,7 +235,6 @@ if (item.type === "nowatermark") {
 videoUrl = item.url;
 break;
 }}
-
 return res.json({
 status: true,
 data: {
@@ -215,17 +242,29 @@ url: videoUrl,
 }, });
 } else if (tag === 'igdl') { // INSTAGRAM
 const response = await axios.get(`https://api.siputzx.my.id/api/d/igdl?url=${url}`);
-
 return res.json({
 status: true,
 data: response.data.data
 });
 } else if (tag === 'fbdl') { // FACEBOOK
 const response = await axios.get(`https://api.siputzx.my.id/api/d/facebook?url=${url}`);
-
 return res.json({
 status: true,
 data: response.data.data
+});
+} else if (tag === 'capcut') { // CAPCUT
+const response = await axios.get(`https://api.siputzx.my.id/api/d/capcut?url=${url}`
+);
+return res.status(200).json({
+status: true,
+data: response.data.data,
+});
+} else if (tag === 'gdrive') { // GDRIVE
+const response = await axios.get(`https://api.siputzx.my.id/api/d/gdrive?url=${url}`
+);
+return res.status(200).json({
+status: true,
+data: response.data.data,
 });
 }
 
